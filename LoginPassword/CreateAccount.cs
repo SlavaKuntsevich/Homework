ef55157a45efa7315aa72152ed7086cb50666118
SlavaKuntsevich <mactep_chif@mail.ru>
2023-03-07 02:07:47 +0300

    Update lesson 10

---
+++ LoginPassword/CreateAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LoginPassword
+{
+    public class CreateAccount
+    {
+        public static bool CreatingAccount(string login, string password, string confirmPassword)
+        {
+            bool registrationSuccses = true;
+            Regex regex = new Regex(@"[0-9]");
+            MatchCollection matches = regex.Matches(password);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches) { }
+            }
+            else
+            {
+                throw new WrongPasswordException("Пароль должен содержать хотя бы 1 цифру");
+            }
+            if (login.Length >= 20) throw new WrongLoginException("Логин не должен превышать 20 символов");
+            if (login.Contains(' ')) throw new WrongLoginException("Логин не должен содержать пробелы");
+            if (password.Length >= 20) throw new WrongPasswordException("Пароль не должен превышать 20 символов");
+            if (password.Contains(' ')) throw new WrongPasswordException("Пароль не должен содержать пробелы");           
+            if (!password.Equals(confirmPassword)) throw new WrongPasswordException("Пароли не совпадают");
+            return registrationSuccses;
+        }
+    }   
+}
---
+++ LoginPassword/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LoginPassword
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool registrationDone = CreateAccount.CreatingAccount("qwerty123", "123qwerty", "123qwerty");
+                if (registrationDone) Console.WriteLine("Регистрация завершена успешно");
+            }
+            catch (WrongLoginException ex) { Console.WriteLine(ex.Message); }
+
+            catch (WrongPasswordException ex) { Console.WriteLine(ex.Message); }
+
+        }
+    }
+}
