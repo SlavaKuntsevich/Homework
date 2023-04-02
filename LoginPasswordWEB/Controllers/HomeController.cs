16f17bb477706922a672043f4bd2b876329f447f
SlavaKuntsevich <mactep_chif@mail.ru>
2023-04-02 21:58:14 +0300

    Update Lesson 16

---
+++ LoginPasswordWEB/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using LoginPasswordWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LoginPasswordWEB.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult LoginError()
+		{
+			return NotFound("Ошибка. Неправильно введен логин или пароль");
+		}
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+		[HttpPost]
+		public RedirectResult Login(string username, string password)
+		{
+			string startUsername = "admin";
+			string startPassword = "abc123";
+			if (startUsername.Equals(username) && startPassword.Equals(password))
+			{
+				return Redirect("~/Home/Index");
+			}
+			else return Redirect("~/Home/LoginError");
+		}
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ LoginPasswordWEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginPasswordWEB.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
